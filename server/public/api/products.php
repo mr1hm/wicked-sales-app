1f8015095d5f2018da7f479efd73540f6bc23bf7
Kevin Ihm <kevinihm@gmail.com>
2019-10-07 19:17:02 -0700

    in progress

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,24 +1,25 @@
 <?php
 
-require_once 'db_connection.php';
 require_once 'functions.php';
+require_once 'db_connection.php';
 
 set_exception_handler('error_handler');
 
 startUp();
 
+if (empty($_GET['id']))
+
 $query = "SELECT * FROM `products`";
 $result = mysqli_query($conn, $query);
 
 if (!$result) {
-  print('error in query' . mysqli_error($conn));
-  exit;
+  throw new Exception('error in query' . mysqli_error($conn));
 }
 
 $output = [];
 
 if (mysqli_num_rows($result) === 0) {
-  exit('no data available');
+  print($output);
 } else {
   while ($row = mysqli_fetch_assoc($result)) {
     $output['data'][] = $row;
