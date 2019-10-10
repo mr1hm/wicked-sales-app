f33921ff855310b21252e8f0aa03d78d8bf4ad0d
Kevin Ihm <kevinihm@gmail.com>
2019-10-09 20:35:14 -0700

    add to cart front end 03 - added price to product list items

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -8,6 +8,7 @@ export default class ProductListItem extends React.Component {
           <img src={this.props.productImg[1]} alt="image" className="cardImage card-img-top" />
           <div className="card-body">
             <h5 className="card-title">{this.props.productName}</h5>
+            <p className="productListItem-price">{`$${this.props.productPrice}`}</p>
             <p className="card-text">{this.props.productInfo}</p>
             <button className="btn btn-primary" onClick={() => this.props.setViewPass('details', { id: this.props.productId })}>Product Details</button>
           </div>
