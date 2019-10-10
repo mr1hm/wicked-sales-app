c41e232b06dd187effde121db7187c64618d22c1
Kevin Ihm <kevinihm@gmail.com>
2019-10-09 20:01:59 -0700

    add to cart front end 03 - revisions

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -29,17 +29,17 @@ class ProductDetails extends React.Component {
             <button className="btn btn-warning mt-3 ml-3" onClick={() => this.props.setView('catalog', {})}>Back to Catalog</button>
           </div>
           <div className="image-wrapper p-3">
-            <img src={this.state.product[0].images[0]} className="productDetailsImg" alt="product"></img>
+            <img src={this.state.product.images[0]} className="productDetailsImg" alt="product"></img>
           </div>
           <div className="col single-post-content-wrapper p-3">
-            <h1>{this.state.product[0].name}</h1>
+            <h1>{this.state.product.name}</h1>
             <br />
             <div className="priceTag">
-              {`$${this.state.product[0].price}`}
+              {`$${this.state.product.price}`}
             </div>
             <br />
             <div className="shortDescription">
-              {this.state.product[0].shortDescription}
+              {this.state.product.shortDescription}
             </div>
             <br /><br />
             <button className="btn btn-success" onClick={() => this.props.addToCart(this.state.product)}>Add To Cart</button>
@@ -47,7 +47,7 @@ class ProductDetails extends React.Component {
           </div>
           <div className="row">
             <div className="col-12 longDescription p-3">
-              {this.state.product[0].longDescription}
+              {this.state.product.longDescription}
             </div>
           </div>
         </article>
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -45,7 +45,12 @@ if (mysqli_num_rows($result) === 0 && $id !== false) {
   }
 }
 
-$json_output = json_encode($output);
-print($json_output);
+if ($id !== false) {
+  $json_output = json_encode($output[0]);
+  print($json_output);
+} else {
+  $json_output = json_encode($output);
+  print($json_output);
+}
 
 ?>
